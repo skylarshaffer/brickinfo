f031dc502442e733a8c0f70719c2e9f1440c87bc
Skylar Shaffer <lfz@skylarshaffer.com>
2024-07-11 22:00:51 -0700

    Added ext folder to commit.

--- brickinfo/scripts/popup.js
+++ brickinfo/scripts/popup.js
@@ -1,29 +1,114 @@
-const $form = document.querySelector("form")
-const $apiKey = document.querySelector('input[name="apiKey"]')
+const $blAffiliateApiKeyForm = document.querySelector("#bl-affiliate-api-key-form")
+const $blCredentialsForm = document.querySelector("#bl-credentials-form")
+const $blAffiliateApiKey = document.querySelector('input[name="bl-affiliate-api-key"]')
+const $blUsername = document.querySelector('input[name="bl-username"]')
 
-async function getApiKey () {
-    $apiKey.value = (await chrome.storage.sync.get("apiKey")).apiKey
+async function getBlAffiliateApiKey () {
+    $blAffiliateApiKey.value = (await chrome.storage.sync.get("blAffiliateApiKey")).blAffiliateApiKey
 }
 
-getApiKey();
+async function getblUsername () {
+    $blUsername.value = (await chrome.storage.sync.get("blUsername")).blUsername
+}
 
-$form.addEventListener('submit',function(event){
+async function getBlNewSessionId () {
+    fetch('https://bricklink.com').then((res) => {
+        if (res.status !== 200) {
+            console.log({word: 'Error', desc: 'There was a problem obtaining the BLNEWSESSIONID cookie.'});
+            return
+        }
+        res.json().then((data) => {
+            const blNewSessionId = data.blNewSessionId
+            return blNewSessionId
+        })
+    }).catch((err) => {
+        console.log({word: 'Error', desc: 'There was a problem obtaining the BLNEWSESSIONID cookie.'})
+        return
+    })
+}
+
+async function validateBlNewSessionId (blNewSessionId, blUsername, blPassword) {
+    fetch('https://www.bricklink.com/ajax/renovate/loginandout.ajax',{
+        method: "POST",
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Cookie': 'BLNEWSESSIONID='
+        },
+        body: {userid: blUsername ,password: blPassword}
+    }).then((res) => {
+        if (res.status !== 200) {
+            console.log({word: 'Error', desc: 'There was a problem validating the BLNEWSESSIONID cookie.'});
+            return
+        } else {
+            console.log({word: 'Success', desc: 'Res status 200, assuming success.'})
+            return
+        }
+    })
+}
+
+async function getColorsXml (blNewSessionId) {
+    fetch('https://www.bricklink.com/catalogDownload.asp?downloadType=X&viewType=3',{
+        headers: {
+          'Cookie': blNewSessionId
+        }
+    }).then((res) => {
+        if (res.status !== 200) {
+            console.log({word: 'Error', desc: 'There was a problem downloading colors.xml.'});
+            return
+        } else {
+            console.log({word: 'Success', desc: 'Res status 200, assuming success.'})
+            console.log({word: 'colors.xml', res: res})
+            return
+        }
+    })
+}
+
+async function getCodesXml (blNewSessionId) {
+    fetch('https://www.bricklink.com/catalogDownload.asp?downloadType=X&viewType=5',{
+        headers: {
+          'Cookie': blNewSessionId
+        }
+    }).then((res) => {
+        if (res.status !== 200) {
+            console.log({word: 'Error', desc: 'There was a problem downloading codes.xml.'});
+            return
+        } else {
+            console.log({word: 'Success', desc: 'Res status 200, assuming success.'})
+            console.log({word: 'codes.xml', res: res})
+            return
+        }
+    })
+}
+
+getBlAffiliateApiKey();
+getBlUsername();
+
+$blAffiliateApiKeyForm.addEventListener('submit',function(event){
     event.preventDefault()
-    const newApiKey = event.target.apiKey.value
+    const newBlAffiliateApiKey = event.target.blAffiliateApiKey.value
     console.log("added to the form")
-    console.log(newApiKey)
-    chrome.storage.sync.set({ apiKey: newApiKey }).then(() => {
+    console.log(newBlAffiliateApiKey)
+    chrome.storage.sync.set({ blAffiliateApiKey: newBlAffiliateApiKey }).then(() => {
         console.log("Value is set");
     });
 })
 
-chrome.storage.sync.onChanged.addListener((changes, namespace) => {
-    for (let [apiKey, { oldValue, newValue }] of Object.entries(changes)) {
-        console.log(
-            `Storage key "${apiKey}" in namespace "${namespace}" changed.`,
-            `Old value was "${oldValue}", new value is "${newValue}".`
-        );
-        $apiKey.value = newValue
-    }
-    
-});
+$blCredentialsForm.addEventListener('submit',function(event){
+    event.preventDefault()
+    const newBlUsername = event.target.blUsername.value
+    chrome.storage.sync.set({ blUsername: newBlUsername }).then(() => {
+        console.log("Bl credentials set");
+    });
+})
+
+$blCredentialsForm.addEventListener('submit',function(event){
+    event.preventDefault()
+    const providedBlUsername = event.target.blUsername.value
+    const providedBlPassword = event.target.blPassword.value
+    getBlNewSessionId((blNewSessionId) =>{
+        validateBlNewSessionId(blNewSessionId, providedBlUsername,providedBlPassword)
+        .then(() => {
+            console.log('Assuming all good')
+        })
+    })
+})
